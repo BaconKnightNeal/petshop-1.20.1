7009c62989e3f74235e38a9c9fcf232172976b12
Neal <staananeal@gmail.com>
2025-03-11 16:58:35 +0800

    Initial commit

---
+++ src/main/java/net/baconknight/petshop/PetShopDataGenerator.java
@@ -0,0 +1,11 @@
+package net.baconknight.petshop;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PetShopDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
